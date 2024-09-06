4a7826c2b850e55a974224a39ca246650d963d7f
popfendi <dino.peer@gmail.com>
2024-09-06 13:53:29 +0100

    encrytion utils refactor

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,13 @@
 import { useState, useEffect } from "react";
+import {
+  hashPassword,
+  deriveKeyFromPassword,
+  encryptMasterKey,
+  decryptMasterKey,
+  arrayBufferToBase64,
+  base64ToArrayBuffer,
+  verifyPassword,
+} from "../utils/Encrypt";
 
 const crypto = window.crypto || window.msCrypto;
 
@@ -98,97 +107,6 @@ const Login = (props) => {
       console.error("Login failed:", error);
       setLoginError(true);
     }
-  }
-
-  function timingSafeEqual(a, b) {
-    if (a.length !== b.length) {
-      return false;
-    }
-
-    let result = 0;
-    for (let i = 0; i < a.length; i++) {
-      result |= a[i] ^ b[i];
-    }
-
-    return result === 0;
-  }
-
-  function base64ToArrayBuffer(base64) {
-    var binaryString = atob(base64);
-    var bytes = new Uint8Array(binaryString.length);
-    for (var i = 0; i < binaryString.length; i++) {
-      bytes[i] = binaryString.charCodeAt(i);
-    }
-    return bytes.buffer;
-  }
-
-  async function hashPassword(password, salt) {
-    const encoder = new TextEncoder();
-    const passwordData = encoder.encode(password);
-    console.log(salt);
-    const hashBuffer = await crypto.subtle.digest(
-      "SHA-256",
-      new Uint8Array([...salt, ...passwordData])
-    );
-    return new Uint8Array(hashBuffer);
-  }
-
-  async function decryptMasterKey(derivedKey, encryptionData) {
-    const iv = base64ToArrayBuffer(encryptionData.keyEncryptionIV);
-    const encryptedKey = base64ToArrayBuffer(encryptionData.encryptedMasterKey);
-    const decryptedKey = await crypto.subtle.decrypt(
-      { name: "AES-GCM", iv: iv },
-      derivedKey,
-      encryptedKey
-    );
-    return new Uint8Array(decryptedKey);
-  }
-
-  async function deriveKeyFromPassword(password, salt) {
-    const encoder = new TextEncoder();
-    const passwordData = encoder.encode(password);
-    const keyMaterial = await crypto.subtle.importKey(
-      "raw",
-      passwordData,
-      { name: "PBKDF2" },
-      false,
-      ["deriveBits", "deriveKey"]
-    );
-    return crypto.subtle.deriveKey(
-      {
-        name: "PBKDF2",
-        salt: salt,
-        iterations: 100000,
-        hash: "SHA-256",
-      },
-      keyMaterial,
-      { name: "AES-GCM", length: 256 },
-      true,
-      ["encrypt", "decrypt"]
-    );
-  }
-
-  async function verifyPassword(password, authData) {
-    const salt = new Uint8Array(base64ToArrayBuffer(authData.salt));
-    const passwordHash = await hashPassword(password, salt);
-    return timingSafeEqual(
-      passwordHash,
-      new Uint8Array(base64ToArrayBuffer(authData.passwordHash))
-    );
-  }
-
-  async function encryptMasterKey(masterKey, keyEncryptionKey) {
-    const iv = crypto.getRandomValues(new Uint8Array(12));
-    const encryptedMasterKey = await crypto.subtle.encrypt(
-      { name: "AES-GCM", iv: iv },
-      keyEncryptionKey,
-      masterKey
-    );
-    return { encryptedMasterKey: new Uint8Array(encryptedMasterKey), iv };
-  }
-
-  function arrayBufferToBase64(buffer) {
-    return btoa(String.fromCharCode.apply(null, new Uint8Array(buffer)));
   }
 
   async function storeAuthData(authData) {
---
+++ src/utils/Encrypt.js
@@ -0,0 +1,120 @@
+const crypto = window.crypto || window.msCrypto;
+
+export async function encryptData(privateKey, masterKey) {
+  const iv = crypto.getRandomValues(new Uint8Array(12));
+  const encryptedPrivateKey = await crypto.subtle.encrypt(
+    { name: "AES-GCM", iv: iv },
+    masterKey,
+    privateKey
+  );
+
+  return {
+    encryptedPrivateKey: arrayBufferToBase64(encryptedPrivateKey),
+    iv: arrayBufferToBase64(iv),
+  };
+}
+
+export async function decryptData(encryptionData, masterKey) {
+  const iv = base64ToArrayBuffer(encryptionData.iv);
+  const encryptedPrivateKey = base64ToArrayBuffer(
+    encryptionData.encryptedPrivateKey
+  );
+
+  const decryptedPrivateKey = await crypto.subtle.decrypt(
+    { name: "AES-GCM", iv: iv },
+    masterKey,
+    encryptedPrivateKey
+  );
+
+  return new Uint8Array(decryptedPrivateKey);
+}
+
+export function base64ToArrayBuffer(base64) {
+  var binaryString = atob(base64);
+  var bytes = new Uint8Array(binaryString.length);
+  for (var i = 0; i < binaryString.length; i++) {
+    bytes[i] = binaryString.charCodeAt(i);
+  }
+  return bytes.buffer;
+}
+
+export function arrayBufferToBase64(buffer) {
+  return btoa(String.fromCharCode.apply(null, new Uint8Array(buffer)));
+}
+
+export function timingSafeEqual(a, b) {
+  if (a.length !== b.length) {
+    return false;
+  }
+
+  let result = 0;
+  for (let i = 0; i < a.length; i++) {
+    result |= a[i] ^ b[i];
+  }
+
+  return result === 0;
+}
+
+export async function hashPassword(password, salt) {
+  const encoder = new TextEncoder();
+  const passwordData = encoder.encode(password);
+  const hashBuffer = await crypto.subtle.digest(
+    "SHA-256",
+    new Uint8Array([...salt, ...passwordData])
+  );
+  return new Uint8Array(hashBuffer);
+}
+
+export async function decryptMasterKey(derivedKey, encryptionData) {
+  const iv = base64ToArrayBuffer(encryptionData.keyEncryptionIV);
+  const encryptedKey = base64ToArrayBuffer(encryptionData.encryptedMasterKey);
+  const decryptedKey = await crypto.subtle.decrypt(
+    { name: "AES-GCM", iv: iv },
+    derivedKey,
+    encryptedKey
+  );
+  return new Uint8Array(decryptedKey);
+}
+
+export async function deriveKeyFromPassword(password, salt) {
+  const encoder = new TextEncoder();
+  const passwordData = encoder.encode(password);
+  const keyMaterial = await crypto.subtle.importKey(
+    "raw",
+    passwordData,
+    { name: "PBKDF2" },
+    false,
+    ["deriveBits", "deriveKey"]
+  );
+  return crypto.subtle.deriveKey(
+    {
+      name: "PBKDF2",
+      salt: salt,
+      iterations: 100000,
+      hash: "SHA-256",
+    },
+    keyMaterial,
+    { name: "AES-GCM", length: 256 },
+    true,
+    ["encrypt", "decrypt"]
+  );
+}
+
+export async function verifyPassword(password, authData) {
+  const salt = new Uint8Array(base64ToArrayBuffer(authData.salt));
+  const passwordHash = await hashPassword(password, salt);
+  return timingSafeEqual(
+    passwordHash,
+    new Uint8Array(base64ToArrayBuffer(authData.passwordHash))
+  );
+}
+
+export async function encryptMasterKey(masterKey, keyEncryptionKey) {
+  const iv = crypto.getRandomValues(new Uint8Array(12));
+  const encryptedMasterKey = await crypto.subtle.encrypt(
+    { name: "AES-GCM", iv: iv },
+    keyEncryptionKey,
+    masterKey
+  );
+  return { encryptedMasterKey: new Uint8Array(encryptedMasterKey), iv };
+}
